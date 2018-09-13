ac30301769fd81b1d0bceac400fca9595f3dfa60
netravler <pheintz@hotmail.com>
2018-09-13 13:33:48 -0500

    Add files via upload

---
+++ Sql_Lite_Tool/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Sql_Lite_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exploreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.getToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schemaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.recordsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(830, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // findToolStripMenuItem
+            // 
+            this.findToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findToolStripMenuItem1,
+            this.exploreToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.findToolStripMenuItem.Name = "findToolStripMenuItem";
+            this.findToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.findToolStripMenuItem.Text = "SQL";
+            // 
+            // findToolStripMenuItem1
+            // 
+            this.findToolStripMenuItem1.Name = "findToolStripMenuItem1";
+            this.findToolStripMenuItem1.Size = new System.Drawing.Size(201, 24);
+            this.findToolStripMenuItem1.Text = "Find Server";
+            this.findToolStripMenuItem1.Click += new System.EventHandler(this.findToolStripMenuItem1_Click);
+            // 
+            // exploreToolStripMenuItem
+            // 
+            this.exploreToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listToolStripMenuItem,
+            this.getToolStripMenuItem});
+            this.exploreToolStripMenuItem.Name = "exploreToolStripMenuItem";
+            this.exploreToolStripMenuItem.Size = new System.Drawing.Size(201, 24);
+            this.exploreToolStripMenuItem.Text = "Explore Databases";
+            this.exploreToolStripMenuItem.Click += new System.EventHandler(this.exploreToolStripMenuItem_Click);
+            // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.listToolStripMenuItem.Text = "List";
+            this.listToolStripMenuItem.Click += new System.EventHandler(this.listToolStripMenuItem_Click);
+            // 
+            // getToolStripMenuItem
+            // 
+            this.getToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.schemaToolStripMenuItem});
+            this.getToolStripMenuItem.Name = "getToolStripMenuItem";
+            this.getToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.getToolStripMenuItem.Text = "Get";
+            // 
+            // schemaToolStripMenuItem
+            // 
+            this.schemaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.schemaToolStripMenuItem1,
+            this.recordsToolStripMenuItem1});
+            this.schemaToolStripMenuItem.Name = "schemaToolStripMenuItem";
+            this.schemaToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.schemaToolStripMenuItem.Text = "Tables";
+            this.schemaToolStripMenuItem.Click += new System.EventHandler(this.schemaToolStripMenuItem_Click);
+            // 
+            // schemaToolStripMenuItem1
+            // 
+            this.schemaToolStripMenuItem1.Name = "schemaToolStripMenuItem1";
+            this.schemaToolStripMenuItem1.Size = new System.Drawing.Size(152, 24);
+            this.schemaToolStripMenuItem1.Text = "Schema";
+            this.schemaToolStripMenuItem1.Click += new System.EventHandler(this.schemaToolStripMenuItem1_Click);
+            // 
+            // recordsToolStripMenuItem1
+            // 
+            this.recordsToolStripMenuItem1.Name = "recordsToolStripMenuItem1";
+            this.recordsToolStripMenuItem1.Size = new System.Drawing.Size(152, 24);
+            this.recordsToolStripMenuItem1.Text = "Records";
+            this.recordsToolStripMenuItem1.Click += new System.EventHandler(this.recordsToolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(201, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(204, 430);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(262, 74);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(556, 396);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(262, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(424, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(530, 46);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "SQL Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "User";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(530, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(636, 47);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(633, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Database";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 482);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SQL Lite Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exploreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem getToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schemaToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem schemaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem recordsToolStripMenuItem1;
+    }
+}
+
---
+++ Sql_Lite_Tool/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace Sql_Lite_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void findToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // Create form explore and have selection passed back...
+
+            List<string> sqlList = new List<string>();
+
+            DataTable dt = SqlDataSourceEnumerator.Instance.GetDataSources();
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (dr["InstanceName"].Equals(string.Empty))
+                {
+                    sqlList.Add(string.Concat(dr["ServerName"], "\\" , Environment.NewLine));
+                }
+                else
+                {
+                    sqlList.Add(string.Concat(dr["ServerName"], "\\", dr["InstanceName"], Environment.NewLine));
+                }
+            }
+
+            richTextBox1.WordWrap = false;
+
+            foreach (string sqlSrvLst in sqlList)
+            {
+                richTextBox1.AppendText(sqlSrvLst);
+            }
+
+            richTextBox1.Select(0, richTextBox1.Lines[0].Length);
+        }
+
+        //
+
+        private void exploreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //select * from master.sys.databases
+            //execute us based upon item from the generated list from the above outcome
+            //SELECT name FROM Sysobjects where xtype = 'u'
+
+            //add sql connect and query below...
+
+            getTopDataBase();
+
+        }
+
+        private void getTopDataBase()
+        {
+            textBox1.Text = richTextBox1.SelectedText.Replace(Environment.NewLine, "");
+            //textBox1.Text = richTextBox1.Lines[0];
+            textBox1.Refresh();
+
+            using (var con = new SqlConnection("Data Source=" +  @textBox1.Text + "; Integrated Security=True;"))
+            {
+                try
+                {
+                    con.Open();
+                    DataTable databases = con.GetSchema("Databases");
+                    con.Close();
+
+                    foreach (DataRow database in databases.Rows)
+                    {
+                        richTextBox2.AppendText(database.Field<String>("database_name") + Environment.NewLine);
+                    }
+                }
+                catch (Exception e)
+                {
+                    richTextBox2.Clear();
+                    richTextBox2.Text = e.ToString();
+                }
+            } 
+        }
+
+        private void listToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //select * from master.sys.databases
+            //execute us based upon item from the generated list from the above outcome
+            //SELECT name FROM Sysobjects where xtype = 'u'
+
+            //add sql connect and query below...
+
+            getTopDataBase();
+        }
+
+        private void schemaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // get the select DB name
+            richTextBox2.Select(0, richTextBox2.Lines[0].Length);
+
+            // connect and get the schema then close...
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+
+            try
+            {
+                List<string> holdit = new List<string>();
+                holdit = grabSchema(textBox3.Text, textBox4.Text);
+
+                foreach (string element in holdit)
+                {
+                    richTextBox1.AppendText(element + "\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.ToString(), "Connection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private List<string> grabSchema(string DTBToGrabSchemaFor, string grabDB)
+        {
+            String ConnectionString;
+            int countIT = 0;
+
+            String[] holdit;
+            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+            String catstring = "";
+            String sqlcommand = "";
+
+            try
+            {
+                List<string> sBase = new List<string>();
+
+                ConnectionString = @"server=PAUL-HEI-LAP\SQLEXPRESS;user=" + textBox2.Text + ";password=" + textBox3.Text + ";Integrated Security = True;database=" + textBox4.Text;
+                SqlConnection cn = new SqlConnection(ConnectionString);
+                cn.Open();
+
+                sqlcommand = "exec sp_columns @table_name = " + DTBToGrabSchemaFor;
+
+                SqlDataAdapter transactions_1 = new SqlDataAdapter(sqlcommand, cn);
+                transactions_1.Fill(dt);
+
+                // Loop through all entries
+                foreach (DataRow drRow in dt.Rows)
+                {
+                    foreach (var item in drRow.ItemArray)
+                    {
+                        catstring = catstring + item + " ";
+                    }
+
+                    holdit = catstring.Split(' ');
+
+                    sBase.Add(holdit[3]);
+
+                    countIT += 1;
+
+                    catstring = "";
+                }
+
+                cn.Close();
+                return sBase;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.ToString(), "Connection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return null;
+        }
+
+        private void recordsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void schemaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = @"server=PAUL-HEI-LAP\SQLEXPRESS;user=" + textBox2.Text + ";password=" + textBox3.Text + ";Integrated Security = True;database=" + textBox4.Text;
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                // Connect to the database then retrieve the schema information.
+                string[] restrictions = new string[4];
+                restrictions[2] = "pB_Acct";
+
+                connection.Open();
+                DataTable table = connection.GetSchema("Tables", restrictions);
+
+                // Display the contents of the table.
+                DisplayData(table);
+            }
+        }
+
+        private void recordsToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayData(System.Data.DataTable table)
+        {
+            foreach (System.Data.DataRow row in table.Rows)
+            {
+                foreach (System.Data.DataColumn col in table.Columns)
+                {
+                    richTextBox2.AppendText(col.ColumnName + Environment.NewLine);
+                }
+            }
+        }
+    }
+}
